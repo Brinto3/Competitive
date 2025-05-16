6f2377ec605159158686cd88635039cf80756e83
Brinto3 <113965654+Brinto3@users.noreply.github.com>
2025-05-16 22:20:08 +0600

    Add files via upload

---
+++ Celcious_To_Farenheit_Converter.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+float cel;
+float fern;
+
+cout<<"Enter temperature in celcius:";
+cin>>cel;
+
+cout<<"Temperature in fahrenheit is:"<< (cel * 9/5)+32;
+return 0;
+}
