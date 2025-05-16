33a182224054217e6b57f53a3e0ee1c24593d3e1
Brinto3 <113965654+Brinto3@users.noreply.github.com>
2025-05-16 17:58:09 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Addition_Substraction_of_Three_Number
+{
+    class Three
+    {
+        static void Main()
+        {
+            int num1 = 11;
+            int num2 = 9;
+            int num3 = 5;
+
+            int Addition = num1 + num2 + num3;
+            Console.WriteLine("The Addition is:" + Addition);
+            int Substraction = num1 - num2 - num3;
+            Console.WriteLine("The Substraction is:" + Substraction);
+        }
+    }
+}
